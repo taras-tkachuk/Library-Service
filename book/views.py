69e89fe9b7a5060d28e15c663a9c52c99001cd9f
Taras Tkachuk <tarastkachuk123@gmail.com>
2024-10-18 16:07:31 +0300

    Add book filter by title & author

--- book/views.py
+++ book/views.py
@@ -7,6 +7,18 @@ from book.serializers import BookSerializer, BookListSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
 
+    def get_queryset(self):
+        title = self.request.query_params.get("title")
+        author = self.request.query_params.get("author")
+
+        queryset = self.queryset
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+        if author:
+            queryset = queryset.filter(author__icontains=author)
+        return queryset
+
     def get_serializer_class(self):
         if self.action == "list":
             return BookListSerializer
--- library_service/urls.py
+++ library_service/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/book/", include("book.urls", namespace="book"))
+    path("api/", include("book.urls", namespace="book"))
 ]
