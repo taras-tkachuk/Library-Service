d2d23816304bedb7d98884d4c945c611ec1189aa
Taras Tkachuk <tarastkachuk123@gmail.com>
2024-10-18 15:43:43 +0300

    Add book model & admin panel

--- book/models.py
+++ book/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    class Cover(models.TextChoices):
+        HARD = "Hard"
+        SOFT = "Soft"
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(max_length=4, choices=Cover.choices)
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(max_digits=5, decimal_places=2)
