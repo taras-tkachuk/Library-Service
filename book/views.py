fe26f7f4d255a7a342ae32b20a954408d6ac8136
Taras Tkachuk <tarastkachuk123@gmail.com>
2024-10-18 15:57:05 +0300

    Add book endpoint

--- book/views.py
+++ book/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from book.models import Book
+from book.serializers import BookSerializer, BookListSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BookListSerializer
+        return BookSerializer
