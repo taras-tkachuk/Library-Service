9f66e7169707e854b4dda9504bba38ea2abfd156
Taras Tkachuk <tarastkachuk123@gmail.com>
2024-10-18 16:44:46 +0300

    Add user endpoint

--- library_service/urls.py
+++ library_service/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("book.urls", namespace="book"))
+    path("api/", include("book.urls", namespace="book")),
+    path("api/users/", include("user.urls", namespace="user")),
 ]
