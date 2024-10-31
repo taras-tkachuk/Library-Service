e0114fd77b8767a90a2b312c2c8ed67d0b9ace16
Taras Tkachuk <tarastkachuk123@gmail.com>
2024-10-31 20:24:06 +0200

    Update borrowing view for admin

--- borrowing/views.py
+++ borrowing/views.py
@@ -21,10 +21,15 @@ class BorrowingViewSet(
 
     def get_queryset(self):
         queryset = self.queryset
+        if self.request.user.is_authenticated:
+            if not self.request.user.is_staff:
+                queryset = queryset.filter(user=self.request.user)
+        else:
+            return None
         user_id = self.request.query_params.get("user_id")
         is_active = self.request.query_params.get("is_active")
 
-        if user_id:
+        if user_id and self.request.user.is_staff:
             queryset = queryset.filter(user=user_id)
 
         if is_active:
